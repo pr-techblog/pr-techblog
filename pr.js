2cd87fa18d22743f216e2f0243da5ab4661b966a
Farzad YZ <farskid@gmail.com>
2017-03-18 04:19:25 +0330

    added js file pr.js

---
+++ pr.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', onLoaded, false);
+
+function onLoaded() {
+  window.pr = window.pr || {
+    methods: {
+      loadURL: loadURL,
+      addClass: addClass,
+      removeClass: removeClass,
+      goToTop: goToTop
+    }
+  }
+}
+
+function loadURL(url) {
+  window.location.href = url
+}
+
+function addClass(className, self) {
+  self.classList.add(className);
+}
+
+function removeClass(className, self) {
+  self.classList.remove(className);
+}
+
+function scroll(x,y) {
+  window.scrollTo(x,y);
+}
+
+function goToTop() {
+  scroll(0,0);
+}
