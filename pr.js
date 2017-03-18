e9cd2904bb457515084c796452ad9a664b03aa9e
Farzad YZ <farskid@gmail.com>
2017-03-19 01:01:55 +0330

    changes to design

--- pr.js
+++ pr.js
@@ -1,15 +1,14 @@
-document.addEventListener('DOMContentLoaded', onLoaded, false);
 
-function onLoaded() {
-  window.pr = window.pr || {
-    methods: {
-      loadURL: loadURL,
-      addClass: addClass,
-      removeClass: removeClass,
-      goToTop: goToTop
-    }
+window.pr = {
+  methods: {
+    loadURL: loadURL,
+    addClass: addClass,
+    removeClass: removeClass,
+    goToTop: goToTop
   }
 }
+
+
 
 function loadURL(url) {
   window.location.href = url
@@ -23,10 +22,10 @@ function removeClass(className, self) {
   self.classList.remove(className);
 }
 
-function scroll(x,y) {
-  window.scrollTo(x,y);
+function scroll(x, y) {
+  window.scrollTo(x, y);
 }
 
 function goToTop() {
-  scroll(0,0);
+  scroll(0, 0);
 }
